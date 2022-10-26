2542ea94b5465f50573a76ddf638d9126177fb2a
MuhammadBorhan <0317371981@student.wub.edu.bd>
2022-10-26 15:48:39 +0600

    database connect

--- server.js
+++ server.js
@@ -2,8 +2,15 @@ const mongoose=require("mongoose");
 const dotenv=require("dotenv").config();
 const colors=require("colors");
 
+// app import
 const app=require("./app");
 
+// database connection
+mongoose.connect(process.env.DATABASE_LOCAL).then(()=>{
+    console.log(`Database connection is successful 🛢` .red.bold);
+  })
+
+// server connect
 const port=process.env.PORT || 8080;
 
 app.listen(port,()=>{
